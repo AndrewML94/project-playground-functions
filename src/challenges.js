078caad1d417793c3e464a0e359d55cb62099895
Andrew Lima <andrewmll94@gmail.com>
2022-08-25 09:28:59 -0300

    Conclusão 9° requisito

--- src/challenges.js
+++ src/challenges.js
@@ -81,11 +81,15 @@ function fizzBuzz(numero) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(palavra1) {
+  let troca1;
+  troca1 = palavra1.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4').replace(/u/g, '5');
+  return troca1;
 }
-function decode() {
-  // seu código aqui
+function decode(palavra2) {
+  let troca2;
+  troca2 = palavra2.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+  return troca2;
 }
 
 // Desafio 10
